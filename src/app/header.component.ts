5597904518686bd0657818d067878f3e59961565
Rubens <rubens_r28@hotmail.com>
2024-07-03 18:45:18 +0100

    Finishing header component style

--- src/app/header.component.ts
+++ src/app/header.component.ts
@@ -2,8 +2,9 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: 'app-header',
-    template: './header.component.html',
     standalone: true,
+    templateUrl: "./header.component.html",
+    styleUrl: './header.component.css'
 })
 export class HeaderComponent {
 
